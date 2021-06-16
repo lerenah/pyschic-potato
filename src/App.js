cca4862cb621643e79450a20b23a71db4835b050
Lerena Holloway <lerenaholloway@Lerenas-MacBook-Air.local>
2021-06-15 17:46:25 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Continuos deployment w/ Travis CLI
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://kubernetes.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bye There!
         </a>
       </header>
     </div>
